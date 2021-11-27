6f6288f4562e1df285958fb99b19b9e4f5e66911
Jim DeVore <jim@jimandicndydevore.com>
2021-11-27 00:38:45 -0500

    WIP

--- run_py_recur_verify.sh
+++ run_py_recur_verify.sh
@@ -11,17 +11,18 @@ echo 'x 2021-01-11 (A) 2021-01-04 test stripping in a completed task with priori
 echo 'x 2021-01-12 2021-01-05 test stripping in a completed task without a priority'                                                         >> todo_verify.txt
 echo 'test stripping projects +project1 +project2 +project3'                                                                                 >> todo_verify.txt
 echo 'test stripping contexts @context1 @context2 @context3'                                                                                 >> todo_verify.txt
+echo 'Replace _elapsed_ with the completion date of the newest match. The done.txt does not have an entry so it will be added'               >> todo_verify.txt
 echo 'Replace _elapsed_ with the completion date of the newest match. The done.txt entry is newer than the recurrence and will not be added' >> todo_verify.txt
 echo 'Replace _elapsed_ with the completion date of the newest match. The done.txt entry is older that the recurrence and will be added'     >> todo_verify.txt
 #
 # two entries to be sure it grabs the newer of the two
-echo 'x 2020-12-01 - Replace _elapsed_ with the completion date of the newest match. The done.txt entry is newer than the recurrence and will not be added'            > done_verify.txt
-echo 'x 2021-02-01 - Replace _elapsed_ with the completion date of the newest match. The done.txt entry is newer than the recurrence and will not be added'           >> done_verify.txt
-echo 'x 2020-12-01 - Replace _elapsed_ with the completion date of the newest match. The done.txt entry is older that the recurrence and will be added'               >> done_verify.txt
+echo 'x 2020-12-01 Replace _elapsed_ with the completion date of the newest match. The done.txt entry is newer than the recurrence and will not be added'            > done_verify.txt
+echo 'x 2021-02-01 Replace _elapsed_ with the completion date of the newest match. The done.txt entry is newer than the recurrence and will not be added'           >> done_verify.txt
+echo 'x 2020-12-01 Replace _elapsed_ with the completion date of the newest match. The done.txt entry is older that the recurrence and will be added'               >> done_verify.txt
 #
 echo
 echo Running tests on py_recur, writing console output to a file for comparison with a master file
-todo.sh -d config py_recur -t -oir="py_recur_verify.txt" -oit="todo_verify.txt" -oid="done_verify.txt -osd=2020-12-31 > run_py_recur_verify_console.txt
+todo.sh -d config py_recur -t -oir="py_recur_verify.txt" -oit="todo_verify.txt" -oid="done_verify.txt" -osd=2020-12-31 > run_py_recur_verify_console.txt
 echo
 echo Running diffs on the saved console output. A pass will only show a different verison number at most.
 echo diff run_py_recur_verify_console_master.txt run_py_recur_verify_console.txt 
